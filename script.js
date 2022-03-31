4bf8122490418bdc1053653741968bc6df40535c
mukhlisa <mukhlis.777.88@gmail.com>
2022-03-31 14:43:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let num1 = prompt('enter number')
+let num2 = prompt('enter number')
+let num3 = prompt('enter number')
+
+    if(num2 < num1 && num3){
+        alert('average number is '+ num1)
+        }else if (num1 < num2 && num3){
+            alert('average number is '+ num2)
+        }else{
+            alert('average number is ' + num3)
+        }
+
